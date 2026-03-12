09c3bdbdb9e63f0a197d035a34f85a6a3dbf8cfb
Mudassir Shehzad <62471417+mudassir957@users.noreply.github.com>
2026-03-12 14:45:57 +0100

    added render CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,7 @@ app = FastAPI(title="Smart Heating System Backend")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://ise-smart-heating-system-frontend.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
