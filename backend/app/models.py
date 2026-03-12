c116bfceb23419c8d48758a79a735ae3dcf6b419
Mudassir Shehzad <62471417+mudassir957@users.noreply.github.com>
2026-03-12 14:27:04 +0100

    fix bug in sensor model

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,12 +5,18 @@ from .database import Base
 
 class SensorData(Base):
     __tablename__ = "sensor_data"
+
     id = Column(Integer, primary_key=True, index=True)
     temperature = Column(Float, nullable=False)
     occupancy = Column(Integer, nullable=False)  # 0/1
-    timestamp = Column(DateTime, default=datetime.datetime.utcnow, index=True, nullable=False)
+    timestamp = Column(
+        DateTime,
+        default=datetime.datetime.utcnow,
+        index=True,
+        nullable=False
+    )
 
-    __table_args__ = (Index("ix_sensor_data_timestamp", "timestamp"),)
+#    __table_args__ = (Index("ix_sensor_data_timestamp", "timestamp"),)
 
 class User(Base):
     __tablename__ = "users"
